f7c200f3e7575fbdff81889f0e083981797aa36c
zhangyukun0521 <308937104@qq.com>
2014-11-04 22:49:27 +0800

    11

---
+++ src/main/java/dxol/util/DebugPrinter.java
@@ -0,0 +1,10 @@
+package dxol.util;
+
+import java.io.PrintStream;
+
+/*
+ * 用于在控制台输出调试信息
+ */
+public class DebugPrinter {
+	public static PrintStream out = System.out;
+}
---
+++ src/main/java/dxol/web/account/SuccessController.java
@@ -0,0 +1,20 @@
+package dxol.web.account;
+
+import org.apache.shiro.SecurityUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import dxol.service.account.ShiroDbRealm.ShiroUser;
+
+@Controller
+@RequestMapping(value = "/success")
+public class SuccessController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String success() {
+
+		ShiroUser user = (ShiroUser) SecurityUtils.getSubject().getPrincipal();
+		return "admin/view/acount/index";
+	}
+}
